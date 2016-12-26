8b47973b8caa4a270cfdd29d983a78613abc921d
kafu17173 <johnsonlee28@gmail.com>
2016-12-26 14:39:10 +0800

    add edit and delete function to myposts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,34 @@ class PostsController < ApplicationController
     end
   end
 
-    private
+  def destroy
+    @post = Post.find(params[:id])
+
+    @post.destroy
+
+    redirect_to account_posts_path
+
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+    @group = Group.find(params[:group_id])
+
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to account_posts_path
+    else
+      render :edit
+    end
+  end
+
+
+
+private
 
     def post_params
       params.require(:post).permit(:content)
